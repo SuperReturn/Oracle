f0d6a012b7559a4f5ef158cfd3ed8e9f26594f32
ChunWang1998 <jjooee1998@gmail.com>
2025-09-16 15:25:13 +0800

    script: deployment script adjustment

--- script/DeploysSuperUSDMorphoOracle.s.sol
+++ script/DeploysSuperUSDMorphoOracle.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.21;
 
 import {Script} from "forge-std/Script.sol";
-import {sSuperUSDOracle} from "../src/sSuperUSDOracle.sol";
+import {sSuperUSDMorphoOracle} from "../src/sSuperUSDMorphoOracle.sol";
 
 contract DeploySSuperUSDOracle is Script {
     function run() external {
@@ -10,7 +10,19 @@ contract DeploySSuperUSDOracle is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        sSuperUSDOracle oracle = new sSuperUSDOracle();
+        // Deploy sSuperUSDOracle contract
+        // Arbitrum Mainnet Addresses
+        address collateralToken = 0x139450C2dCeF827C9A2a0Bb1CB5506260940c9fd;  // sSuperUSD
+        address loanToken = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;        // USDC
+        address oracleAddress = 0xdF206c6Ebb600365A47889DF1A0C691a83fa58b0;    // sSuperUSD Oracle
+        address fallbackOracleAddress = 0xdF206c6Ebb600365A47889DF1A0C691a83fa58b0;    // sSuperUSD Oracle
+
+        sSuperUSDMorphoOracle oracle = new sSuperUSDMorphoOracle(
+            collateralToken,
+            loanToken,
+            oracleAddress,
+            fallbackOracleAddress
+        );
 
         // Stop recording transactions
         vm.stopBroadcast();
