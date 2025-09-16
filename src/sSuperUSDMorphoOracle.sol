145704d07bfc20404080f1304bc7b9241fc84a02
ChunWang1998 <jjooee1998@gmail.com>
2025-09-16 10:24:37 +0800

    chore: dependency and solc version

--- src/sSuperUSDMorphoOracle.sol
+++ src/sSuperUSDMorphoOracle.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
 
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
